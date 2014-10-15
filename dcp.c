826247c92a8853f544f8a554fa7b451b7ac7ef4f
William Morriss <wjmelements@gmail.com>
2014-10-14 23:25:49 -0500

    argc

--- dcp.c
+++ dcp.c
@@ -1,3 +1,11 @@
-int main() {
+#include <stdio.h>
+static const char* USAGE ="\
+usage: dcp SRC DST\n\
+";
+int main(int argc, char* argv[]) {
+    if (argc != 3) {
+        fprintf(stderr, USAGE);
+        return -1;
+    }
     return 0;
 }
