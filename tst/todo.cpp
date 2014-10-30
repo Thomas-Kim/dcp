8b0e6075c6799b70fea8e49438fdeac4d67c8306
William Morriss <wjmelements@gmail.com>
2014-10-30 18:11:03 -0500

    more rigorous test

--- tst/todo.cpp
+++ tst/todo.cpp
@@ -10,32 +10,46 @@
 using std::set;
 #include <string>
 using std::string;
+#include <vector>
+using std::vector;
 
-int main() {
+// in actual use there will be at most one producer per directory but this helps protect correctness
+void* put_path(void* arg) {
+    const char* str = (const char*) arg;
+    add_path(str);
+    return NULL;
+}
+void trial() {
     set<string> paths;
     DIR* dir = opendir(".");
     if (dir == NULL) {
         perror(".");
-        return errno;
+        exit(errno);
     }
     todo_init();
     struct dirent* ent;
+    vector<pthread_t> children;
     do {
         ent = readdir(dir);
         if (ent == NULL) {
             if (errno) {
                 perror("readdir");
-                return errno;
+                exit(errno);
             }
         } else {
             string put(ent->d_name);
             paths.insert(put);
+            pthread_t child;
             size_t sz = put.length() + 1;
             char* copy = (char*) malloc(sz);
             strncpy(copy, ent->d_name, sz);
-            add_path(copy);
+            pthread_create(&child, NULL, put_path, copy);
+            children.push_back(child);
         }
     } while(ent);
+    for (auto it = children.cbegin(); it != children.cend(); it++) {
+        pthread_join(*it, NULL);
+    }
     set<string> popped;
     const char* pop;
     while (1) {
@@ -49,4 +63,12 @@ int main() {
         }
     }
     assert(popped == paths);
+    closedir(dir);
+}
+
+int main() {
+    for (size_t i = 0; i < 500; i++) {
+        trial();
+    }
+    return 0;
 }
