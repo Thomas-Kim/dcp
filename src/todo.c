39533fd052671b7e8de4bbde05d9b41278ffa978
William Morriss <wjmelements@gmail.com>
2014-11-11 14:44:57 -0600

    templates with c preprocessor

--- src/todo.c
+++ src/todo.c
@@ -12,26 +12,81 @@
 
 const size_t buf_size = 0x400; // Buffer size for paths
 
-struct todo {
+typedef struct todo {
     struct stat info;
-    const char* path;
+    char* path;
     #ifdef __cplusplus
     // how to order items
     bool operator < (const todo& other) const {
         return info.st_size > other.info.st_size;
     };
     #endif // __cplusplus
-};
+} todo_t;
+typedef todo_t* todo_t_ptr;
 
 // TODO restructure so that end is a pointer to and invalid node and not a pointer to a pointer
-static struct mpsc {
-    struct node {
-        struct node* next;
-        // strictly increasing count
-        size_t count; // XXX overflow
-        struct todo* data;
-    } **begin, **end, *start;
-} dirs, files;
+#define mpsc(type)\
+struct mpsc##type {\
+    struct node##type  **begin, **end, *start;\
+};\
+struct node##type {\
+    struct node##type* next;\
+    size_t count; \
+    type data;\
+};\
+\
+size_t count_from##type(struct node##type** target) {\
+    struct node##type* node = (struct node##type*) target;\
+    size_t prev_count = node->count;\
+    return prev_count;\
+}\
+static void update_tail##type(struct mpsc##type* mpsc, size_t count, struct node##type** proposed) {\
+    size_t proposed_count = count_from##type(proposed);\
+    while (1) {\
+        struct node##type** current_tail = mpsc->end;\
+        size_t current_count = count_from##type(current_tail);\
+        if (current_count > proposed_count) {\
+            return;\
+        }\
+        if (atomic_compare_exchange_strong(&mpsc->end, &current_tail, proposed)) {\
+            return;\
+        }\
+    }\
+}\
+\
+static void put##type(struct mpsc##type* mpsc, type todo) {\
+    struct node##type* put = malloc(sizeof(struct node##type));\
+    put->data = todo;\
+    put->next = NULL;\
+    struct node##type** target;\
+    struct node##type* expected;\
+    try_node:\
+    target = mpsc->end;\
+    expected = NULL;\
+    put->count = count_from##type(target);\
+    if (!atomic_compare_exchange_strong(target, &expected, put)) {\
+        goto try_node;\
+    }\
+    update_tail##type(mpsc, put->count, &put->next);\
+}\
+static void clean_one##type(struct mpsc##type* mpsc) {\
+    struct node##type* to_free = mpsc->start;\
+    mpsc->start = to_free->next;\
+    free(to_free);\
+}\
+\
+static type get##type(struct mpsc##type* mpsc) {\
+    if (*mpsc->begin) {\
+        struct node##type* got = *mpsc->begin;\
+        type ret = got->data;\
+        mpsc->begin = &got->next;\
+        clean_one##type(mpsc);\
+        return ret;\
+    }\
+    return NULL;\
+}\
+struct mpsc##type
+mpsc(todo_t_ptr) dirs, files;
 volatile size_t available, in_progress;
 void todo_init() {
     // TODO a constructor-like thing?
@@ -54,82 +109,37 @@ void todo_destroy() {
     free(files.start);
 }
 
-// target must be a pointer to a "next" pointer
-size_t count_from(struct node** target) {
-    // the count follows the next ptr
-    struct node* node = (struct node*) target;
-    size_t prev_count = node->count;
-    return prev_count;
-}
-static void update_tail(struct mpsc* mpsc, size_t count, struct node** proposed) {
-    size_t proposed_count = count_from(proposed);
-    while (1) {
-        struct node** current_tail = mpsc->end;
-        size_t current_count = count_from(current_tail);
-        if (current_count > proposed_count) {
-            return;
-        }
-        if (atomic_compare_exchange_strong(&mpsc->end, &current_tail, proposed)) {
-            return;
-        }
-    }
-}
 
-static void put(struct mpsc* mpsc, struct todo* todo) {
-    struct node* put = malloc(sizeof(struct node));
-    put->data = todo;
-    put->next = NULL;
-    struct node** target;
-    struct node* expected;
-    try_node: // TODO restructure as loop
-    target = mpsc->end;
-    expected = NULL;
-    put->count = count_from(target);
-    if (!atomic_compare_exchange_strong(target, &expected, put)) {
-        goto try_node;
-    }
-    update_tail(mpsc, put->count, &put->next);
-}
-static void clean_one(struct mpsc* mpsc) {
-    struct node* to_free = mpsc->start;
-    mpsc->start = to_free->next;
-    free(to_free);
-}
-
-static const char* get(struct mpsc* mpsc, struct stat* info) {
-    if (*mpsc->begin) {
-        struct node* got = *mpsc->begin;
-        memcpy(info, &got->data->info, sizeof(*info));
-        const char* ret = got->data->path;
-        free(got->data);
-        mpsc->begin = &got->next;
-        clean_one(mpsc);
-        return ret;
-    }
-    return NULL;
-}
 
 
 const char* get_next(struct stat* info) {
-    const char* ret = get(&dirs, info);
-    if (ret) {
-        return ret;
+    struct todo* got = gettodo_t_ptr(&dirs);
+    if (!got) {
+        got = gettodo_t_ptr(&files);
+        if (!got) {
+            return NULL;
+        }
     }
-    ret = get(&files, info);
+    size_t n = strlen(got->path) + 1;
+    char* ret = malloc(n);
+    strncpy(ret, got->path, n);
+    memcpy(info, &got->info, sizeof(*info));
+    free(got->path);
+    free(got);
     return ret;
 }
 
-void add_file(const char* path, struct stat* info) {
+void add_file(char* path, struct stat* info) {
     struct todo* data = malloc(sizeof(struct todo));
     memcpy(&data->info, info, sizeof(*info));
     data->path = path;
-    put(&files, data);
+    puttodo_t_ptr(&files, data);
 }
-void add_dir(const char* path, struct stat* info) {
+void add_dir(char* path, struct stat* info) {
     struct todo* data = malloc(sizeof(struct todo));
     memcpy(&data->info, info, sizeof(*info));
     data->path = path;
-    put(&dirs, data);
+    puttodo_t_ptr(&dirs, data);
 }
 void add_path(const char* path) {
     struct stat info;
